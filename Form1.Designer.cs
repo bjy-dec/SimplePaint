b010313bd6fa518db9846bd4a169e46ab03756af
배 재용 <bjy7012@naver.com>
2026-04-30 16:56:59 +0900

    과제2 코딩과 테스트 완료  PictureBox의 MouseDown, MouseMove, MouseUp 이벤트를 이용해서 도형 그리기 구현 도형 종류에 따라 직선, 사각형, 원을 그리는 로직 구현 선택한 색상과 선 굵기를 적용해서 도형 그리기 구현 도형 그리기 중 점선으로 도형 모양 보이도록 구현

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -184,7 +184,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.White;
+            BackColor = SystemColors.Control;
             ClientSize = new Size(800, 578);
             Controls.Add(picCanvas);
             Controls.Add(groupBox3);
